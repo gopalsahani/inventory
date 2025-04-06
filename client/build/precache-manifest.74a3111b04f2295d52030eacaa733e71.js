c58781f7b4482fdcbd1dd12e8b5732ea1c700773
gopalsahani <135799987+gopalsahani@users.noreply.github.com>
2025-04-06 11:41:15 +0530

    2 commit
    no

---
+++ client/build/precache-manifest.74a3111b04f2295d52030eacaa733e71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b0e231dd6aefa1e6090947fdcda696",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73ec681eb3e721ed233d",
+    "url": "/static/css/main.fc702b39.chunk.css"
+  },
+  {
+    "revision": "0941626569ecb392eb1c",
+    "url": "/static/js/2.39d09558.chunk.js"
+  },
+  {
+    "revision": "60db3800776cbc38682f",
+    "url": "/static/js/3.d08649bd.chunk.js"
+  },
+  {
+    "revision": "73ec681eb3e721ed233d",
+    "url": "/static/js/main.4ca96a20.chunk.js"
+  },
+  {
+    "revision": "b8db357f917851c47f2b",
+    "url": "/static/js/runtime~main.f68af9a8.js"
+  }
+]);
