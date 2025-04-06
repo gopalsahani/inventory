fc3714109c04a13150b28c37e9e7fcb5abb19fc8
gopalsahani <135799987+gopalsahani@users.noreply.github.com>
2025-04-06 11:32:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,18 +29,18 @@ app.get("/", (req, res) => {
 
 
 */
-//static files
-app.use(express.static(path.join(__dirname,"./client/build")));
-app.get("*",function(req,res){
-  res.sendFile(path.join(__dirname,"./client/build/index.html"))
-});
+
+
 app.use("/api/v1/admin", require("./routes/Adminroutes"));
 app.use("/api/v1/tech", require("./routes/Techroutes"));
 app.use("/api/v1/tech/lab", require("./routes/Labroutes"));
 app.use("/api/v1/tech/teacher",require("./routes/Teacherroute"));
 app.use("/api/v1/tech/server",require("./routes/Serverroute"));
-
-
+//static files
+app.use(express.static(path.join(__dirname,"./client/build")));
+app.get("*",function(req,res){
+  res.sendFile(path.join(__dirname,"./client/build/index.html"))
+});
 app.listen(port, (req, res) => {
   console.log(
     `server is running on ${process.env.NODE_MODE} mode on port no ${process.env.PORT}`
